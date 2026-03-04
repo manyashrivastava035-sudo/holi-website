db34e60e33ebc7e66e7b468bb2888426cea80e21
manyashrivastava035-sudo <manyashrivastava035@gmail.com>
2026-03-04 17:03:21 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,1 +1,25 @@
+function startColors() {
+    for (let i = 0; i < 80; i++) {
+        let color = document.createElement("div");
+        color.classList.add("color");
 
+        let colors = [
+            "#ff4e50",
+            "#fc913a",
+            "#f9d423",
+            "#00c9ff",
+            "#92fe9d",
+            "#ff00de"
+        ];
+
+        color.style.background = colors[Math.floor(Math.random() * colors.length)];
+        color.style.left = Math.random() * window.innerWidth + "px";
+        color.style.animationDuration = (Math.random() * 2 + 2) + "s";
+
+        document.body.appendChild(color);
+
+        setTimeout(() => {
+            color.remove();
+        }, 4000);
+    }
+}
